5b6978602be6ed2e94d5dbec14fa7a7eb9ea1233
Garry Ing <garry.ing@gmail.com>
2022-04-06 17:53:16 -0400

    deps update

--- cli/cmd/ui/resources/src/static/js/index.js
+++ cli/cmd/ui/resources/src/static/js/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './components/app'
+const container = document.getElementById('root')
+const root = createRoot(container)
 
-ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById('root')
-)
+root.render(<HashRouter><App /></HashRouter>)
 
 // const index = {
 //   about: function (html) {
